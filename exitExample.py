e59d1208194ebbbdf60425179bc4c2eb0672f079
Kyle Davis <kyleRdavis-me@nau.edu>
2019-05-27 12:42:07 -0700

    Add files via upload

---
+++ exitExample.py
@@ -0,0 +1,10 @@
+#Kyle Davis - 5-27-2019
+
+import sys
+
+while True:
+    print('Type exit to exit.')
+    response = input()
+    if response == 'exit':
+        sys.exit()
+print('You typed ' + response + '.')
---
+++ printRandom.py
@@ -0,0 +1,6 @@
+#Kyle Davis - 5-27-2019
+
+import random
+
+for i in range(5):
+    print(random.randint(1,10))
