924482849ca5fb4a5234e104533d147f19c9406b
Kyle Davis <kyleRdavis-me@nau.edu>
2019-05-27 12:15:54 -0700

    Add files via upload

---
+++ yourName.py
@@ -0,0 +1,7 @@
+#Kyle Davis - 5-27-2019
+
+name = ' '
+while name != 'your name':
+    print('Please type your name.')
+    name = input()
+print('Thank you!')
---
+++ yourName2.py
@@ -0,0 +1,10 @@
+#Kyle Davis - 5-27-2019
+
+name = ' '
+while name != 'your name':
+    print('Please type your name.')
+    name = input()
+    if name == 'your name':
+        break
+    
+print('Thank you!')
